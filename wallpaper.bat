07e167b7acad209539d29837d26eaa94656ed942
Royce Whitaker <roycewhitaker@rocketmail.com>
2020-01-07 11:04:26 -0800

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,1 +1,10 @@
-//File
+@echo off
+
+bitsadmin /create WallpaperScriptDownload
+bitsadmin /transfer WallpaperScriptDownload /download /priority normal http://sjsu-as-ptc.github.io/wallpaper/files.txt c:\wallpaper\files.txt
+
+bitsadmin /create Wallpaper
+SET "FILENAME=%~dp0\name.ext"
+bitsadmin /addfileset wallpaper c:\wallpaper\files.txt
+bitsadmin /transfer wallpaper /download /priority normal c:\wallpaper\%filename%
+pause 5
