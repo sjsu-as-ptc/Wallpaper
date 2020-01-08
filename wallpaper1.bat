37736e6041a47e2cbf27b981e093a088fa190dc9
Royce Whitaker <roycewhitaker@rocketmail.com>
2020-01-08 10:28:16 -0800

    Update wallpaper1.bat

--- wallpaper1.bat
+++ wallpaper1.bat
@@ -29,9 +29,8 @@ IF NOT "%_os%"=="10" (
 
 endlocal
 
-
 bitsadmin /transfer DownloadWget /download /priority normal http://sjsu-as-ptc.github.io/wallpaper/wget.exe c:\wallpaper\wget.exe
-
+cd c:\wallpaper
 start wget.exe -N -q -O c:\wallpaper\files.txt http://sjsu-as-ptc.github.io/wallpaper/files.txt c:\wallpaper\files.txt
 timeout 5
 rem copy wget.exe c:\wallpaper\wget.exe
