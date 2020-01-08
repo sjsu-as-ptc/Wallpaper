2e6d360b934991bfa87e382041a53de8c80769ca
Royce Whitaker <roycewhitaker@rocketmail.com>
2020-01-08 09:10:51 -0800

    Update wallpaper1.bat

--- wallpaper1.bat
+++ wallpaper1.bat
@@ -1,7 +1,9 @@
 @echo off
 
+bitsadmin /transfer DownloadWget /download /priority normal http://sjsu-as-ptc.github.io/wallpaper/wget.exe c:\wallpaper\wget.exe
+
 start wget.exe -N -q -O c:\wallpaper\files.txt http://sjsu-as-ptc.github.io/wallpaper/files.txt c:\wallpaper\files.txt
 timeout 5
-copy wget.exe c:\wallpaper\wget.exe
+rem copy wget.exe c:\wallpaper\wget.exe
 cd c:\wallpaper
 start wget.exe -N -q -i c:\wallpaper\files.txt
